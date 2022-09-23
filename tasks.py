474acc4e542ce88ddc82301681478dcd265ac12e
diogo <dipm@novonordisk.com>
2022-09-23 11:23:26 +0000

    dev: add task runner step to clean project

--- tasks.py
+++ tasks.py
@@ -30,3 +30,23 @@ def docs(ctx, step='build'):
         ]
 
     ctx.run(';'.join(cmds))
+
+@task
+def clean(ctx):
+    """Clear the project of ignore files.
+
+    :param ctx: _description_
+    :type ctx: _type_
+    """
+    cmds = [
+        "rm -rf node_modules",
+        "rm -f lib/*.js",
+        "rm -f lib/*.js.map",
+        "rm -f bin/*.js",
+        "rm -f bin/*.js.map"
+        "rm -f test/*.js",
+        "rm -f test/*.js.map"
+    ]
+    ctx.run(';'.join(cmds))
+
+    docs(ctx, 'rm')
